8ab337d3a6975b49a009196759c409fc0d14b26d
AmilaViduranga <amilavidurangawelikala@gmail.com>
2016-09-08 07:38:04 +0530

    add git ignore

--- testWithsequelize/app.js
+++ testWithsequelize/app.js
@@ -55,4 +55,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
+var server = app.listen(8002, function() {
+  console.log('Server listening on port ' + server.address().port);
+});
 module.exports = app;
